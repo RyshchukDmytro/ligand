1be55040cbe8f20135c6a2cbdd6cfcae6cd31aeb
Dmytro Ryshchuk <dmytro.ryshchuk@icloud.com>
2019-10-13 23:03:39 +0300

    show launch screen for 1.2 second

--- SwiftProtein/Others/AppDelegate.swift
+++ SwiftProtein/Others/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Thread.sleep(forTimeInterval: 1.2) // use to show launch screen for 1.2 second
         return true
     }
 
